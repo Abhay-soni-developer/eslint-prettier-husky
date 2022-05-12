ff36d5e3e83955181b4ddad19545e6745ee6c52a
abhay <abhay.soni@patchinfotech.com>
2022-05-13 00:02:10 +0530

    husky installed

--- index.js
+++ index.js
@@ -13,15 +13,4 @@ console.log(b);
 /* Non-letter hero */
 
 const a = 5;
-
-const promise = new Promise((resolve, reject) => {
-  if (a === 5) {
-    resolve(true);
-  } else {
-    reject(new Error("number not equal to 5"));
-  }
-});
-
-promise.then(console.log).catch((err) => {
-  console.log(err);
-});
+console.log(a + 2);
